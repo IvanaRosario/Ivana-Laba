d898eb78bf0e048997f4fca5a4bdf4b9291a1bab
IvanaRosario <151400879+IvanaRosario@users.noreply.github.com>
2024-09-02 19:22:03 -0300

    Moved to Maven

---
+++ University/src/main/java/com/solvd/laba/interfaces/IAttendable.java
@@ -0,0 +1,7 @@
+package com.solvd.laba.interfaces;
+
+public interface IAttendable {
+    void attend(boolean isPresent);
+    int getAttendanceCount();
+    // la aplique en las tres hijas de persona de la misma manera
+}
---
+++ University/src/main/java/com/solvd/laba/interfaces/IIdentifiable.java
@@ -0,0 +1,9 @@
+package com.solvd.laba.interfaces;
+
+public interface IIdentifiable {
+
+    void getInformation();
+
+
+
+}
